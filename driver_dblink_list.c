6f240c2fbec6d853ed06ad6be293a4c8b35471d6
Max <hoeselm@sec.in.tum.de>
2015-06-18 16:56:08 +0200

    Readding

---
+++ driver_dblink_list.c
@@ -0,0 +1,20 @@
+/*
+ * driver_dblink_list.c
+ *
+ *  Created on: Jun 3, 2015
+ *      Author: hoeselm
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "dblink_list.h"
+
+int main(void){
+	long array[4];
+	array[0] = 1;
+	array[1] = 7;
+	array[2] = 15;
+	array[3] = 69;
+	print_list((create_sorted_list(array,4)));
+	return 0;
+}
